5463477d654b7bd193f368f64ffb39c7b9fe49b9
tofique207 <tofiquej786@gmail.com>
2025-10-05 22:00:40 +0530

    Added Student route(only add_student)

--- app/models.py
+++ app/models.py
@@ -1,8 +1,16 @@
 from app import db
 from flask_login import UserMixin
+from datetime import datetime
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(100), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(200), nullable=False)
+
+class Student(db.Model):
+    id=db.Column(db.Integer,primary_key=True)
+    name=db.Column(db.String(100),nullable=False)
+    email=db.Column(db.String(120),unique=True,nullable=False)
+    course=db.column(db.Integer)
+    created_at=db.Column(db.DateTime,default=datetime.utcnow)
--- app/routes/students.py
+++ app/routes/students.py
@@ -1,7 +1,34 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, request, redirect, url_for, flash
+from app import db
+from app.models import Student
+from flask_login import login_required
 
 students_bp=Blueprint('students',__name__,url_prefix='/students')
 
 @students_bp.route('/')
+@login_required
 def student_home():
-    return "Student Home Page"
+    students=Student.query.all()
+    return render_template('students/list.html')
+
+@students_bp.route('/add',methods=["GET",'POST'])
+@login_required
+def add_student():
+    if request.method=='POST':
+        name=request.form.get('name')
+        email=request.form.get('email')
+        course=request.form.get('course')
+        age=request.form.get('age')
+
+        existing_student=Student.query.filter_by(email=email).first()
+        if existing_student:
+            flash("Student Already Exists")
+            return redirect(url_for("students.add_student"))
+        
+        new_student=Student(name=name, email=email, course=course, age=age)
+        db.session.add(new_student)
+        db.session.commit()
+        flash("Student Added Successfully")
+        return redirect(url_for("students.student_home"))
+    
+    return render_template('students/add.html')
