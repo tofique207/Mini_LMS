eb53645b46590133971ffed656648e6a15de2a00
tofique207 <tofiquej786@gmail.com>
2025-10-06 21:43:58 +0530

    updated students table

--- app/models.py
+++ app/models.py
@@ -9,8 +9,9 @@ class User(db.Model, UserMixin):
     password = db.Column(db.String(200), nullable=False)
 
 class Student(db.Model):
-    id=db.Column(db.Integer,primary_key=True)
-    name=db.Column(db.String(100),nullable=False)
-    email=db.Column(db.String(120),unique=True,nullable=False)
-    course=db.Column(db.Integer)
-    created_at=db.Column(db.DateTime,default=datetime.utcnow)
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    age = db.Column(db.Integer)
+    course = db.Column(db.String(100))
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
